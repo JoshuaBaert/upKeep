8f1420b321451faf804d91631930127c963e0498
Joshua Baert <jbghoring@gmail.com>
2016-12-01 12:18:50 -0700

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Joshua Baert on 12/1/2016.
+ */
+
+module.exports = {
+	secret : "pLxRYYma2pZfb6ypYjhoAzCymxJBQ7Ve",
+	googleClientSecret: "cteYsn47UD61yjtC74Kf2Xaz",
+	googleClientId: "320717460113-dfpdp0ir299971iivpfbrke21acsleo3.apps.googleusercontent.com"
+};
