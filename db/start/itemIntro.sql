86a787a299ebeaf53649a2966d0468b2c2fb6321
Joshua Baert <jbghoring@gmail.com>
2016-12-09 23:33:44 -0700

    Added few more polished items.. made intro items for new users

--- db/start/itemIntro.sql
+++ db/start/itemIntro.sql
@@ -4,3 +4,5 @@ VALUES ('Creating', 1481180400000, 'The + button on the bottom right will create
 ('Settings', 1481180400000, 'The gear icon in the bottom left will allow you to change information about you, and how you are okay with being notified', $1, $2),
 ('Editing', 1481180400000, 'The pencil icon will allow you to edit the item it is display with.', $1, $2),
 ('Reminders', 1481180400000, 'Reminders will be send out the day before the date of the reminder.. They will be sent as SMS or Email depending on your settings.. By default SMS will be disabled..', $1, $2);
+('Special Note', 1481180400000, 'Current known bugs. 1. The icon field categories is incomplete (you can try random words by placing "fa-" infront of them example "fa-home, fa-list, fa-car" I will be fixing this 2. Sometimes you will need to refresh or click off then back on a page to see new reminders.', $1, $2),
+('Thanks', 1481180400000, 'First thanks for testing this app for me!! Please let me know any issues or bugs you find so that I may fix them (Developer@baert.io incase you do not have my email)', $1, $2);
