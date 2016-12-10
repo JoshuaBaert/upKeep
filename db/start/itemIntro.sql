4d33a6cb497329bc1e7f07282a3f89b72e3fb78f
Joshua Baert <jbghoring@gmail.com>
2016-12-09 23:14:31 -0700

    Added few more polished items.. made intro items for new users

--- db/start/itemIntro.sql
+++ db/start/itemIntro.sql
@@ -3,4 +3,4 @@ VALUES ('Creating', 1481180400000, 'The plus button (Bottom-right) will create a
 ('Navigation', 1481180400000, 'The buttons on the left are the nav bar. The logo will take you to a view of all of your catagories. Each individual list will create a new button on the left side. The plus on the nav bar will always create a new catagory', $1, $2),
 ('Settings', 1481180400000, 'The gear icon in the bottom left will allow you to change information about you, and how you are okay with being notified', $1, $2),
 ('Editing', 1481180400000, 'The pencil icon will allow you to edit the item it is display with.', $1, $2),
-('Reminders', 1481180400000, 'Reminders will be send out the day before.. They will be sent as SMS or Email depending on your settings.. By default SMS will be disabled..', $1, $2);
+('Reminders', 1481180400000, 'Reminders will be send out the day before the date of the reminder.. They will be sent as SMS or Email depending on your settings.. By default SMS will be disabled..', $1, $2);
