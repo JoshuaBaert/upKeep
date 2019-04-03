4cc3e72f2f1e697bcf8afc2612ce8cca1be7cb6c
JoshuaBaert <developer@baert.io>
2019-04-03 12:51:32 -0500

    initally setup

--- public/js/app.js
+++ public/js/app.js
@@ -8,40 +8,40 @@ angular.module('upKeep', ['ui.router', 'ngMaterial'])
 		$stateProvider
 			.state('login', {
 				url: '/login',
-				templateUrl: './views/login.html'
+				templateUrl: './views/login.html',
 			})
 			.state('user', {
 				url: '/',
 				templateUrl: './views/user.html',
 			})
 			.state('user.settings', {
-				url: "settings",
-				templateUrl: './views/settings.html'
+				url: 'settings',
+				templateUrl: './views/settings.html',
 			})
 			.state('user.home', {
-				url: "",
-				templateUrl: './views/home.html'
+				url: '',
+				templateUrl: './views/home.html',
 			})
-				.state('user.home.new', {
-					url: "home",
-					templateUrl: './views/side/newList.html'
-				})
-				.state('user.home.edit', {
-					url: 'home/edit/:listIndex',
-					templateUrl: './views/side/editList.html',
-					reload: true
-				})
+			.state('user.home.new', {
+				url: 'home',
+				templateUrl: './views/side/newList.html',
+			})
+			.state('user.home.edit', {
+				url: 'home/edit/:listIndex',
+				templateUrl: './views/side/editList.html',
+				reload: true,
+			})
 			.state('user.list', {
 				url: '',
-				templateUrl: './views/list.html'
+				templateUrl: './views/list.html',
 			})
-				.state('user.list.new', {
-					url: 'list/:listIndex',
-					templateUrl: './views/side/newItem.html'
-				})
-				.state('user.list.edit', {
-					url: 'list/:listIndex/:itemIndex',
-					templateUrl: './views/side/editItem.html'
-				});
+			.state('user.list.new', {
+				url: 'list/:listIndex',
+				templateUrl: './views/side/newItem.html',
+			})
+			.state('user.list.edit', {
+				url: 'list/:listIndex/:itemIndex',
+				templateUrl: './views/side/editItem.html',
+			});
 		$urlRouterProvider.otherwise('/login');
 	});
