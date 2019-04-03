c4b65c620475515b9e8be15a66dba0b76e1d9804
JoshuaBaert <developer@baert.io>
2019-04-03 13:51:48 -0500

    save

--- public/src/directives.js
+++ public/src/directives.js
@@ -5,64 +5,58 @@
 var openSpeed = 500;
 var openWidth = '100vw';
 
-angular.module('upKeep')
-    .directive('getUser', function () {
-        return {
-            restrict: 'E',
-            link: function (scope, element, attrs) {
-                scope.getUser();
-            },
-        };
-    })
-    .directive('closeCreate', function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                $(element).on('click', function () {
-                    $('.side-panel').css('width', '0');
-                });
-            },
-        };
-    })
-    .directive('openCreate', function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                $(element).on('click', function () {
-                    setTimeout(function () {
-                        $('.side-panel').css('width', openWidth);
-                    }, openSpeed);
-                });
-            },
-        };
-    })
-    .directive('autoOpenCreate', function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                $(document).ready(function () {
-                    setTimeout(function () {
-                        $('.side-panel').css('width', openWidth);
-                    }, openSpeed);
-                });
+angular.module('upKeep').directive('getUser', function () {
+    return {
+        restrict: 'E',
+        link: function (scope, element, attrs) {
+            scope.getUser();
+        },
+    };
+}).directive('closeCreate', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            $(element).on('click', function () {
+                $('.side-panel').css('width', '0');
+            });
+        },
+    };
+}).directive('openCreate', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            $(element).on('click', function () {
+                setTimeout(function () {
+                    $('.side-panel').css('width', openWidth);
+                }, openSpeed);
+            });
+        },
+    };
+}).directive('autoOpenCreate', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            $(document).ready(function () {
+                setTimeout(function () {
+                    $('.side-panel').css('width', openWidth);
+                }, openSpeed);
+            });
 
-            },
-        };
-    })
-    .directive('getUser', function () {
-        return {
-            restrict: 'EA',
-            link: function (scope, element, attrs) {
-                scope.getUser();
-            },
-        };
-    })
-    .directive('datePicker', function () {
-        $('.date-picker').datepicker({
-            changeMonth: true,
-            changeYear: true,
-        });
+        },
+    };
+}).directive('getUser', function () {
+    return {
+        restrict: 'EA',
+        link: function (scope, element, attrs) {
+            scope.getUser();
+        },
+    };
+}).directive('datePicker', function () {
+    $('.date-picker').datepicker({
+        changeMonth: true,
+        changeYear: true,
     });
+});
 
 
 
